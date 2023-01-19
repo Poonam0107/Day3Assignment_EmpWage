f10f7fbc49cf3e22374a4bc53994ca05a768f42c
Poonam Mane <manepoonam001@gmail.com>
2023-01-19 17:39:09 +0530

    Welcome to EmployeeWage

---
+++ UC1_EmpWage.java
@@ -0,0 +1,18 @@
+package Day3Assignment;
+
+public class UC1_EmpWage {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int empCheck=1;
+		int Is_Full_Time=1;
+		double check=Math.floor(Math.random()*10%2);
+		if(empCheck==Is_Full_Time)
+			System.out.println("Employee is Present");
+			else
+				System.out.println("Employee is Absene");
+
+	}
+
+}
